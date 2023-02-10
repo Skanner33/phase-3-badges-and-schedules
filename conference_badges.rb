9a050c3ee89269cb32959e094afddb7389e2a37d
Skanner33 <mathewmageto@gmail.com>
2023-02-10 10:35:21 +0300

    completed

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker(name)
+   return "Hello, my name is #{name}."
+end    
+
+def batch_badge_creator(array)
+    new_array = []
+    array.each do |element|
+        new_array.push("Hello, my name is #{element}.")
+    end
+    return new_array
+end        
+
+def assign_rooms(array)
+    room = []
+    array.each.with_index do |name, index|
+        room.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+    end
+    return room    
+end
+
+def printer(array)
+    batch_badge_creator(array).each{|element| puts element}
+    assign_rooms(array).each{|element| puts element}
+end    
